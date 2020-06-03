b2a5ce04b7ac6baa2e52e78fd47bf1959bf4b0c7
bangyeonju <31052671+bangyeonju@users.noreply.github.com>
2020-06-03 16:10:25 +0900

    060 3

---
+++ 01_자료형과 기본입출력/src/Ex01.java
@@ -0,0 +1,55 @@
+
+public class Ex01 {
+	public static void main (String[] args) {
+
+		System.out.println(1); // 한문장 뒤에는 꼭 ;(세미콜론)을 쓴다
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println();
+		System.out.println("abc");
+		System.out.println("abc"+"xyz"); //문자열 + 문자열 = 문자열 연결 
+//		System.out.println(abc);  문자출력시 ""로 둘러싸야한다.
+//		System.out.println('abc');  2글자이상의 문자는 "로 둘러싸야한다.
+		System.out.println("a");
+		System.out.println('a');
+
+		 System.out.println();
+		 System.out.println(1+2);
+		 System.out.println("1+2");
+//		 System.out.println('1+2'); 2글자이상의 문자는 "로 둘러싸야한다.
+		 
+		 System.out.println("1"+"2");
+		 System.out.println();
+		 System.out.println("1+2=3");
+		 System.out.println("1+2="+ 1+2);
+		 System.out.println("1+2="+ (1+2));
+		 
+		 System.out.println();
+		 
+		 
+	}
+}
+
+
+
+
+
+
+
+
+/* 
+   복붙 :ctrl + alt + 방향키
+   잘+붙 : alt + 방향키 
+ */
+ 
+
+
+
+
+
+
+
+
+
+
---
+++ 01_자료형과 기본입출력/src/Ex02_정수형.java
@@ -0,0 +1,58 @@
+
+public class Ex02_정수형 {
+	public static void main(String[] args) {
+		System.out.println(10);
+
+		int a, b; // 선언
+		// int b;
+
+		a = 1; // 초기화
+		b = 2;
+
+		int c = 3;
+
+		System.out.println('a');
+		System.out.println("a");
+		System.out.println(a);
+		System.out.println(a + 10);
+
+		System.out.println("a=" + a); // 문자열 + 변수
+		System.out.println("a+b=" + (a + b));
+		System.out.println();
+
+		byte b1 = 10;
+		byte b2 = 20;
+		byte b3;
+		System.out.println("b1=" + b1);
+		System.out.println("b2=" + b2);
+		// System.out.println("b3="+b3);
+
+		// b3 = b1+b2; // b3 =30; byte + byte = int로 자동형변환
+		int result;
+		result = b1 + b2;
+		System.out.println("result=" + result);
+		System.out.println("result=" + (b1 + b2));
+		
+		int i1 = 1000000;
+		int i2 = 2000000;
+//		int i3 = i1 * i2;
+//		long i3 = i1* i2;
+		long i3 = (long)i1 * i2;
+		System.out.println("i3="+i3); 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
